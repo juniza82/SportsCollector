1ab2c5a079f6b70e5d56f3011247e23cfa9df658
ryu <fbwotjq@gmail.com>
2015-01-16 22:39:26 +0900

    log4j2 commit

--- src/main/java/com/namja/collector/SportsMetaDataCollectorServiceImpl.java
+++ src/main/java/com/namja/collector/SportsMetaDataCollectorServiceImpl.java
@@ -5,23 +5,24 @@ import java.lang.management.MemoryMXBean;
 import java.lang.management.MemoryUsage;
 import java.lang.management.OperatingSystemMXBean;
 import java.lang.management.ThreadMXBean;
-import java.text.SimpleDateFormat;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 @EnableScheduling
 public class SportsMetaDataCollectorServiceImpl implements SportsMetaDataCollectorService {
-
-	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
-
+	
+	private static Logger logger = LoggerFactory.getLogger(SportsMetaDataCollectorServiceImpl.class);
+	
     @Scheduled(fixedRate = 5000)
     @Override
     public void reportCurrentDeamonStatus() {
     	
-    	System.out.println(String.format("[DEAMON-MONITOR] ##########################################################################################"));
+    	logger.info(String.format("[DEAMON-MONITOR] ##########################################################################################"));
     	ThreadMXBean threads = ManagementFactory.getThreadMXBean();
-        System.out.println(String.format("[DEAMON-MONITOR] THREAD INFO:: ThreadCount:%s, TotalStartedThreadCount:%s, DaemonThreadCount:%s, PeakThreadCount:%s", 
+    	logger.info(String.format("[DEAMON-MONITOR] THREAD INFO:: ThreadCount:%s, TotalStartedThreadCount:%s, DaemonThreadCount:%s, PeakThreadCount:%s", 
                  threads.getThreadCount(), threads.getTotalStartedThreadCount(), threads.getDaemonThreadCount(), threads.getPeakThreadCount()));
           
          // cpu 모니터링 
@@ -33,9 +34,9 @@ public class SportsMetaDataCollectorServiceImpl implements SportsMetaDataCollect
          MemoryMXBean memBean = ManagementFactory.getMemoryMXBean() ;
          MemoryUsage heap = memBean.getHeapMemoryUsage();
          MemoryUsage nonHeap = memBean.getNonHeapMemoryUsage();
-         System.out.println(String.format("[DEAMON-MONITOR] Heap    : Init: %d, Used: %d, Committed: %d, Max.: %d", heap.getInit(), heap.getUsed(), heap.getCommitted(), heap.getMax()));
-         System.out.println(String.format("[DEAMON-MONITOR] Non-Heap: Init: %d, Used: %d, Committed: %d, Max.: %d", nonHeap.getInit(), nonHeap.getUsed(), nonHeap.getCommitted(), nonHeap.getMax()));
-         System.out.println(String.format("[DEAMON-MONITOR] ##########################################################################################")); 
+         logger.info(String.format("[DEAMON-MONITOR] Heap    : Init: %d, Used: %d, Committed: %d, Max.: %d", heap.getInit(), heap.getUsed(), heap.getCommitted(), heap.getMax()));
+         logger.info(String.format("[DEAMON-MONITOR] Non-Heap: Init: %d, Used: %d, Committed: %d, Max.: %d", nonHeap.getInit(), nonHeap.getUsed(), nonHeap.getCommitted(), nonHeap.getMax()));
+         logger.info(String.format("[DEAMON-MONITOR] ##########################################################################################")); 
     }
 
 }
